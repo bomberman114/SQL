c289dec1b7fb9ce136cece61419a3dd286036836
bomberman114 <mielmer016@gmail.com>
2022-08-25 19:02:16 +0900

    2022/08/25 4장 실습

---
+++ 제4장 SQL 고급 .sql
@@ -0,0 +1,168 @@
+#날짜 : 2022/08/25
+#이름 : 조현길
+#내용 : 제4장 SQL 고급
+
+#실습 4-1
+CREATE TABLE `Member` (
+	`uid`	VARCHAR(10) PRIMARY KEY,
+	`name`	VARCHAR(10) NOT NULL,
+	`hp`	CHAR(13) UNIQUE NOT NULL,
+	`pos`	VARCHAR(10) default '사원',
+	`dep`	TINYINT,
+	`rdate`	DATETIME  NOT NULL
+);
+
+CREATE TABLE `Department` (
+	`depNo`	TINYINT PRIMARY KEY,
+	`name`	VARCHAR(10) NOT NULL,
+	`tel`	CHAR(12) NOT NULL
+);
+
+CREATE TABLE `Sales` (
+	`seq`	INT AUTO_INCREMENT PRIMARY KEY,
+	`uid`	VARCHAR(10) NOT NULL,
+	`year`	YEAR NOT NULL,
+	`month`	TINYINT NOT NULL,
+	`sale`	INT NOT NULL
+);
+#실습 4-2
+INSERT INTO `Member` VALUES ('a101', '박혁거세', '010-1234-1001', '부장', 101, NOW());
+INSERT INTO `Member` VALUES ('a102', '김유신',   '010-1234-1002', '차장', 101, NOW());
+INSERT INTO `Member` VALUES ('a103', '김춘추',   '010-1234-1003', '사원', 101, NOW());
+INSERT INTO `Member` VALUES ('a104', '장보고',   '010-1234-1004', '대리', 102, NOW());
+INSERT INTO `Member` VALUES ('a105', '강감찬',   '010-1234-1005', '과장', 102, NOW());
+INSERT INTO `Member` VALUES ('a106', '이성계',   '010-1234-1006', '차장', 103, NOW());
+INSERT INTO `Member` VALUES ('a107', '정철',     '010-1234-1007', '차장', 103, NOW());
+INSERT INTO `Member` VALUES ('a108', '이순신',   '010-1234-1008', '부장', 104, NOW());
+INSERT INTO `Member` VALUES ('a109', '허균',     '010-1234-1009', '부장', 104, NOW());
+INSERT INTO `Member` VALUES ('a110', '정약용',   '010-1234-1010', '사원', 105, NOW());
+INSERT INTO `Member` VALUES ('a111', '박지원',   '010-1234-1011', '사원', 105, NOW());
+
+INSERT INTO `Department` VALUES (101, '영업1부', '051-512-1001');
+INSERT INTO `Department` VALUES (102, '영업2부', '051-512-1002');
+INSERT INTO `Department` VALUES (103, '영업3부', '051-512-1003');
+INSERT INTO `Department` VALUES (104, '영업4부', '051-512-1004');
+INSERT INTO `Department` VALUES (105, '영업5부', '051-512-1005');
+INSERT INTO `Department` VALUES (106, '영업지원부', '051-512-1006');
+INSERT INTO `Department` VALUES (107, '인사부', '051-512-1007');
+
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a101', 2018, 1,  98100);
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a102', 2018, 1, 136000);
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a103', 2018, 1,  80100);
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a104', 2018, 1,  78000);
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a105', 2018, 1,  93000);
+
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a101', 2018, 2,  23500);
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a102', 2018, 2, 126000);
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a103', 2018, 2,  18500);
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a105', 2018, 2,  19000);
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a106', 2018, 2,  53000);
+
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a101', 2019, 1,  24000);
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a102', 2019, 1, 109000);
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a103', 2019, 1, 101000);
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a104', 2019, 1,  53500);
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a107', 2019, 1,  24000);
+
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a102', 2019, 2, 160000);
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a103', 2019, 2, 101000);
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a104', 2019, 2,  43000);
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a105', 2019, 2,  24000);
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a106', 2019, 2, 109000);
+
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a102', 2020, 1, 201000);
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a104', 2020, 1,  63000);
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a105', 2020, 1,  74000);
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a106', 2020, 1, 122000);
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a107', 2020, 1, 111000);
+
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a102', 2020, 2, 120000);
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a103', 2020, 2,  93000);
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a104', 2020, 2,  84000);
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a105', 2020, 2, 180000);
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a108', 2020, 2,  76000);
+#실습 4-3
+ select * from `member` where `name`='김유신';
+ select * from `member` where `pos`='차장' and dep=101;
+ select * from `member` where `pos`='차장' OR dep=101;
+ select * from `Member` where `name` != '김춘추';
+ select * from `Member` where `name` <> '김춘추';
+ select * from `member` where `pos` ='사원' OR `pos`='대리';
+ select * from `Member` where `pos` in('사원', '대리');
+ select * from `Member` where `pos` in(101, 102, 103);
+ select * from `Member` where `name` like '%신';
+ select * from `Member` where `name` like '김%';
+ select * from `Member` where `name` like '김__';
+ select * from `Member` where `name` like '_성';
+ select * from `Member` where `name` like '정_';
+ select * from `Member` where `name` like '정%';
+ select * from `sales` where `sale` > 50000;
+ SELECT * FROM `Sales` WHERE `sale` >= 50000 AND `sale` < 100000 AND `month`=1;
+ select * from `Sales` where `sale` between 50000 and 100000;
+ select * from `sales` where `sale` not between 50000 and 100000;
+ SELECT * FROM `Sales` WHERE `year` IN(2020); 
+ SELECT * FROM `Sales` WHERE `month` IN(1, 2);
+
+ 
+#실습 4-4
+SELECT * FROM `Sales` ORDER BY `sale`;
+SELECT * FROM `Sales` ORDER BY `sale` ASC;
+SELECT * FROM `Sales` ORDER BY `sale` DESC;
+SELECT * FROM `Member` ORDER BY `name`;
+SELECT * FROM `Member` ORDER BY `name` DESC;
+SELECT * FROM `Member` ORDER BY `rdate` ASC;
+SELECT * FROM `Sales` WHERE `sale` > 50000 ORDER BY `sale` DESC;
+SELECT * FROM `Sales` 
+WHERE `sale` > 50000 
+ORDER BY `year`, `month`, `sale` DESC;
+
+
+#실습 4-5
+select * from `Sales` limit 3;
+select * from `Sales` limit 0,3;
+select * from `Sales` limit 1,2;
+select * from `Sales` limit 5,3;
+select * from `Sales` order by `sale` desc limit 3,5;
+select * from `Sales` where `sale` < 50000 order by `slae` desc limit 3;
+select * from `Sales`
+where `sale` > 50000
+order by `year` desc, `month`, `sale` desc
+limit 5;
+
+#실습 4-6
+SELECT SUM(sale) AS `합계` FROM `Sales`;
+SELECT AVG(sale) AS `평균` FROM `Sales`;
+SELECT MAX(sale) AS `최대값` FROM `Sales`;
+SELECT MIN(sale) AS `최소값` FROM `Sales`;
+SELECT COUNT(`seq`) AS `갯수` FROM `Sales`;
+SELECT COUNT(*) AS `갯수` FROM `Sales`;
+
+select substring(`hp`, 10, 4) as `휴대폰 끝자리` from `Member`;
+
+insert into `Member` values ('b101', '을지문덕', '010-5555-1234', '사장', 107, NOW());
+
+
+#실습 4-7
+select sum(`sale`)as `2018년도 1월 매출총합` from `Sales` where `year` = 2018 and `month` =1;
+
+#실습 4-8
+select 
+	sum(`sale`) as `총합`,
+    avg(`sale`) as `평균`
+from `Sales` 
+where `year`=2019 and `month`=2 and `sale` >= 50000;
+
+#실습 4-9
+select 
+min(`sale`) as `최저`,
+max(`sale`) as `최고` 
+from `Sales` where `year` = 2020;
+
+#실습 4-10
+#실습 4-11
+#실습 4-12
+#실습 4-13
+#실습 4-14
+#실습 4-15
+#실습 4-16
+#실습 4-17
